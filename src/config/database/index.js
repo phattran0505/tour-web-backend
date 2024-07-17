38724a6174a92df2792741f8e23669052c9d3f8f
phattran <phattran052004@gmail.com>
2024-07-17 22:58:19 +0700

    change link mongodb_connection

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-async function connect(){
-    try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/tour-management")
-        console.log("connect to database successful");
-    }
-    catch{
-        console.log("connect failed");
-    }
+dotenv.config();
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION);
+    console.log("connect to database successful");
+  } catch {
+    console.log("connect failed");
+  }
 }
 
-module.exports = {connect}
+module.exports = { connect };
